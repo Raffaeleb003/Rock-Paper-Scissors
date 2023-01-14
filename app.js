94f16942ae02f20a44ff20cb02b4c6905e088aa1
Raffaele Balsamo <raffob003@gmail.com>
2023-01-14 21:46:08 +0100

    Choices Added

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
  userChoice = e.target.id
  userChoiceDisplay.innerHTML = userChoice
+ generateComputerChoice()
 })) 
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+
+    if (randomNumber === 1) {
+        computerChoice = 'Rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'Paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
